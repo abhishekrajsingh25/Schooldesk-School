17858c12cbec0f7368960c5dce21009bc354d270
Abhishek Raj Singh <abhishekrajsingh2509@gmail.com>
2025-07-27 10:05:24 +0530

    School Management System

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -61,6 +61,9 @@ const LoginPage = () => {
           >
             Sign In
           </SignIn.Action>
+          <h3 className="text-gray-500">
+            Enter "admin" in both username and password.
+          </h3>
         </SignIn.Step>
       </SignIn.Root>
     </div>
