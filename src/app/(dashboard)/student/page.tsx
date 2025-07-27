aa47952feacf7ca2f177c13a477d9ca0f2cc5364
Abhishek Raj Singh <abhishekrajsingh2509@gmail.com>
2025-07-27 09:56:36 +0530

    School Management System

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -20,7 +20,12 @@ const StudentPage = async () => {
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Schedule (4A)</h1>
-          <BigCalendarContainer type="classId" id={classItem[0].id} />
+          {/* <BigCalendarContainer type="classId" id={classItem[0].id} /> */}
+          {classItem.length > 0 ? (
+            <BigCalendarContainer type="classId" id={classItem[0].id} />
+          ) : (
+            <p className="text-red-500">No class found for this student.</p>
+          )}
         </div>
       </div>
       {/* RIGHT */}
